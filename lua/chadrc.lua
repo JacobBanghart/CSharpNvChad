c14428169ae910198f9b1f26cacd1623d4fd5f2a
BANGJ018 <jacob.banghart@disney.com>
2025-07-28 15:27:15 -0500

    feat: remove csharp and add typescript

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,7 +7,6 @@ local M = {}
 
 M.base46 = {
 	theme = "tokyodark",
-
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,25 +13,17 @@ return {
     end,
   },
   {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<C-y>",
-          accept_word = "<C-.>",
-        }
-      })
-    end,
-    lazy = false
-  },
-  {
     "ahmedkhalf/project.nvim",
     config = function()
-      require("project_nvim").setup({})
+      require("project_nvim").setup {}
     end,
-    lazy = false
-  }
-
+    lazy = false,
+  },
+  {
+    "github/copilot.vim",
+    config = function() end,
+    lazy = false,
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
